3fe21141c82a6d18619853b36f1a303b1b5da200
Zack Raycraft <zackraycraft@gmail.com>
2018-02-11 20:40:30 -0500

    Lab01

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lab01
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World from Zachary Raycraft");
+        }
+    }
+}
