7b77f2b89c40a72bb51e5341a5e1b8cd17935917
zraycraft <36213774+zraycraft@users.noreply.github.com>
2018-05-15 00:05:57 -0400

    Lab03ZackRaycraft

---
+++ Lab03ZackRaycraft.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lab03ZackRaycraft
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter another number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sumab = a + b;
+            Console.WriteLine(sumab);
+
+
+
+            int x = 1;
+            int y = 2;
+            int z = 3;
+            int sumxyz = (x + y) * (z + 10);
+            Console.WriteLine(sumxyz);
+
+            Console.WriteLine("Hello\tWorld!");
+            Console.WriteLine("Hello\nWorld!");
+            Console.WriteLine("\"Hello World!\" ");
+            Console.WriteLine("Hello // World!");
+        }
+    }
+}
