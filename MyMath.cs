746d70f73318b1d86cb5f1f7ffe9e6e4e4fba2c0
zraycraft <36213774+zraycraft@users.noreply.github.com>
2018-05-14 22:35:09 -0400

    Lab09ZackRaycraft

---
+++ MyMath.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace InClass09
+{
+    public class MyMath
+    {
+        public int result;
+        public int operand1;
+        public int operand2;
+
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtract(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+    }
+}
